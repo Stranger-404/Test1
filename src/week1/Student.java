38c66ffd294549eb757caa21085780b5a0346804
Stranger-404 <khan776@sheridancollege.ca>
2025-02-20 17:00:25 -0500

    Initial COmmit

---
+++ src/week1/Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week1;
+
+/**
+ *
+ * @author AHMAD
+ */
+public class Student {
+    private String name;
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String given_name){
+        name=given_name;
+    }
+    
+}
---
+++ src/week1/Week1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week1;
+
+/**
+ *
+ * @author AHMAD
+ */
+public class Week1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentList =  new Student[1];
+        studentList[0] = new Student();
+        studentList[0].setName("Jibran");
+        
+        for(Student s: studentList){
+            System.out.println(s.getName());
+        }
+        
+    }
+    
+}
